c5a89253e48a36970dbd93ea82e43dcc954b2f7f
Joshua D. Boyd <jdboyd@jdboyd.net>
2013-04-13 02:26:13 -0400

    WIP.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='corker',
-      version='0.1.1',
+      version='0.1.3test',
       description='Another WSGI Framework',
       long_description=open('README.md').read(),
       license='BSD',
@@ -9,6 +9,7 @@ setup(name='corker',
       author_email='jdboyd@jdboyd.net',
       url='https://github.com/jd-boyd/corker',
       packages=find_packages(),
+      package_data={'': ['README', 'LICENSE.txt']},
       install_requires=['webob', 'routes'],
       tests_require=['nose', 'webtest'],
      )
